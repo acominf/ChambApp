828e5afcca92bc50a2f60b80fe8e78f1e09cb401
Bernardo Bernal <supermasterspark100@gmail.com>
2017-05-25 21:15:38 -0500

    Actualizacion de clases
    Implementacion de la clase para la escritura y lectura de archivos.
    Actualización clase usuario

---
+++ Proyecto/src/proyecto/Archivo.java
@@ -0,0 +1,89 @@
+/**
+ * Clase encargada de la escritura y lectura de archivos de la aplicacion.
+ */
+import java.io.*;
+
+public class Archivo {
+    private Usuario user;
+    private String nombreArchivoS;
+    private String nombreArchivoE;
+    private FileOutputStream nuevousuario = null;
+    private ObjectOutputStream fichero = null;
+    private ObjectInputStream entrada = null;
+    private FileInputStream usuario;
+
+    /**
+     * Constructor utilizado para inicializar el usuario a escribir
+     * @param user Parametro que recibe un usuario para escribirlo en archivo
+     */
+    public Archivo(Usuario user){
+        this.user = user;
+        nombreArchivoS = this.user.devuelveNombre() + ".dat";
+    }
+    /**
+     * Constructor utilizado para inicializar el nombre del archivo a leer
+     * @param nombre Equivalente al nombre de usuario para la lectura de archivo
+     */
+    public Archivo(String nombre){
+        nombreArchivoE = nombre + ".dat";
+    }
+
+    /**
+     * Método que se encarga de escribir en un archivo un objeto usuario
+     */
+    public void escribeArchivo(){
+        try {
+            nuevousuario = new FileOutputStream(nombreArchivoS);
+            fichero = new ObjectOutputStream(nuevousuario);
+            fichero.writeObject(user);
+        }
+        catch (FileNotFoundException e){
+            System.out.println(e.getMessage());
+        }
+        catch(IOException e){
+            System.out.println(e.getMessage());
+        }
+        finally {
+            try{
+                if(nuevousuario != null)
+                    nuevousuario.close();
+                if(fichero != null)
+                    fichero.close();
+            }
+            catch (IOException e){
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+    /**
+     * Método que se encarga de leer de un archivo un objeto usuario
+     */
+    public Usuario leeArchivo(){
+        try {
+            usuario = new FileInputStream(nombreArchivoE);
+            entrada = new ObjectInputStream(usuario);
+            user = (Usuario) entrada.readObject();
+        }
+        catch (FileNotFoundException e){
+            System.out.println(e.getMessage());
+        }
+        catch(IOException e){
+            System.out.println(e.getMessage());
+        }
+        catch(ClassNotFoundException e){
+            System.out.println(e.getMessage());
+        }
+        finally {
+            try{
+                if(usuario != null)
+                    usuario.close();
+                if(fichero != null)
+                    fichero.close();
+            }
+            catch (IOException e){
+                System.out.println(e.getMessage());
+            }
+        }
+        return user;
+    }
+}
--- Proyecto/src/proyecto/Usuario.java
+++ Proyecto/src/proyecto/Usuario.java
@@ -1,14 +1,18 @@
-package proyecto;
-
 /**
 *Clase encargada de la creacion de un nuevo usuario
 *@author José Bernardo Bernal Cabrera
 */
-public class Usuario{
+import java.io.Serializable;
+public class Usuario implements Serializable{
 	private String nombre;
-	private String aMaterno;
-	private String aPaterno;
-	public Usuario(){
+	private String pregunta;
+	private String respuesta;
+    private String password;
+	public Usuario(String nombre,String password, String pregunta, String respuesta){
+		this.nombre = nombre;
+		this.pregunta = pregunta;
+		this.respuesta = respuesta;
+		this.password = password;
 	} 
 	/**
 	*Método encargado de la modificacion de datos del usuario
@@ -17,4 +21,32 @@ public class Usuario{
 		
 	}
 
+    /**
+     * Metodo de acceso al atributo nombre de un usuario
+     * @return nombre del usuario
+     */
+	public String devuelveNombre(){
+		return nombre;
+	}
+    /**
+     * Metodo de acceso al atributo pregunta de un usuario
+     * @return pregunta de seguridad del usuario
+     */
+    public String devuelvePregunta(){
+        return pregunta;
+    }
+    /**
+     * Metodo de acceso al atributo respuesta de un usuario
+     * @return respuesta a la pregunta de seguridad del usuario
+     */
+    public String devuelveRespuesta(){
+        return respuesta;
+    }
+    /**
+     * Metodo de acceso al atributo contraseña de un usuario
+     * @return contraseña del usuario
+     */
+    public String devuelvePassword(){
+        return password;
+    }
 }
